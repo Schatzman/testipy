f6ed8279933511b05e29f284127a10a85254db4b
Schatzman <djschatzmiller@gmail.com>
2015-08-29 15:25:02 -0600

    add back utils

---
+++ util/db.py
@@ -0,0 +1,14 @@
+import subprocess
+import sys
+
+def create_db(filename):
+    _file = open(filename,'w+')
+    _file.close()
+
+def delete_db(filename):
+    create_db(filename)
+    if sys.platform == 'win32':
+        subprocess.call(['del',filename],shell=True)
+
+## TODO
+# add method to detect if db exists
